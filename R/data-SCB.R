6527841643e52ec7451c78c873fa4d7d787cc828
Max Gordon <max@gforge.se>
2018-05-26 10:50:30 -0700

    Changed the pxweb ref

--- R/data-SCB.R
+++ R/data-SCB.R
@@ -1,9 +1,9 @@
 #' Average age in Sweden
 #'
 #' For the vignettes there is a dataset downloaded by using the
-#' \code{\link[pxweb]{get_pxweb_data}()} call. The data is from
+#' \code{get_pxweb_data()} call. The data is from
 #' SCB (\href{http://scb.se/}{Statistics Sweden}) and downloaded
-#' using:
+#' using the \href{https://github.com/rOpenGov/pxweb}{pxweb package}:
 #'
 #' @example inst/examples/data-SCB_example.R
 #'
