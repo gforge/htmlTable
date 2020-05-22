8e5be1cfc5a69a483e5b9e4aa31a228fba6dfdf9
Max Gordon <max@gforge.se>
2020-05-22 08:00:18 +0200

    Added tidyHtmlTable tests

---
+++ tests/testthat/test-tidyHtmlTable.R
@@ -0,0 +1,58 @@
+library('testthat')
+library('dplyr')
+library('XML')
+library('tibble')
+library('purrr')
+library('glue')
+
+# Add rownames
+test_that("Basic tidyHtmlTable functionality", {
+  mx <- tribble(~value, ~header, ~name, ~rgroup, ~cgroup1, ~cgroup2,
+                     1,       2,     3,       1,        1,        3,
+                     2,       3,     4,       1,        2,        3,
+                     3,       4,     5,       2,        2,        4) %>%
+    mutate_at(vars(starts_with("cgroup")), ~glue("{name} cg", name = .)) %>%
+    mutate(rgroup = glue("{name} rg", name = rgroup),
+           header = glue("{name} h", name = header))
+  table_str <- mx %>%
+    tidyHtmlTable(header = header,
+                  label = "test_table")
+  parsed_table <- readHTMLTable(as.character(table_str))[["test_table"]]
+  expect_equal(ncol(parsed_table), 4)
+  expect_equal(nrow(parsed_table), length(mx$value))
+
+  table_str <- mx %>%
+    tidyHtmlTable(header = header,
+                  rgroup = rgroup,
+                  label = "test_table")
+
+  parsed_table <- readHTMLTable(as.character(table_str))[["test_table"]]
+  expect_equal(ncol(parsed_table), 4)
+  expect_equal(nrow(parsed_table), length(mx$value) + length(mx$rgroup %>% unique))
+
+  table_str <- mx %>%
+    tidyHtmlTable(header = header,
+                  rgroup = rgroup,
+                  cgroup = cgroup1,
+                  label = "test_table")
+
+  parsed_table <- readHTMLTable(as.character(table_str))[["test_table"]]
+  expect_equal(colnames(parsed_table) %>% keep(~grepl("[0-9]", .)) %>% length,
+               unique(mx$header) %>% length)
+  expect_equal(ncol(parsed_table), 5)
+  expect_equal(nrow(parsed_table), length(mx$value) + length(mx$rgroup %>% unique))
+
+  table_str <- mx %>%
+    tidyHtmlTable(header = header,
+                  rgroup = rgroup,
+                  cgroup = starts_with("cgroup"),
+                  label = "test_table")
+
+  parsed_table <- readHTMLTable(as.character(table_str))[["test_table"]]
+  expect_equal(colnames(parsed_table) %>% keep(~grepl("[0-9]", .)) %>% length,
+               unique(mx$header) %>% length)
+  # Each cgroup generates a empty cell in-between which is how we detect the
+  # cgroup as it adds these for layout purpose
+  expect_equal(ncol(parsed_table), 3 + 1 + 2)
+  expect_equal(nrow(parsed_table), length(mx$value) + length(mx$rgroup %>% unique) + 2)
+})
