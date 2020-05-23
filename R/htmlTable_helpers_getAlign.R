59dc324f16e189af36b3bc3176bfd0aa209780bb
Max Gordon <max@gforge.se>
2020-05-23 22:35:41 +0200

    Fixed bad vertical border with align, e.g. "lrr|r"

--- R/htmlTable_helpers_getAlign.R
+++ R/htmlTable_helpers_getAlign.R
@@ -1,10 +1,11 @@
 #' Gets alignment
 #'
 #' @param index The index of the align parameter of interest
+#' @param emptyCell We don't want two borders when encontering an empty cgroup filler cell
 #' @family hidden helper functions for
 #' @keywords internal
-#' @inheritParams htmlTable
-prGetAlign <- function(align, index) {
+#' @inheritParams addHtmlTableStyle
+prGetAlign <- function(align, index, emptyCell = FALSE) {
   segm_rgx <- "[^lrc]*[rlc][^lrc]*"
 
   res_align <- align
@@ -18,11 +19,18 @@ prGetAlign <- function(align, index) {
     lrc_data <- substr(res_align, 1, rmatch + attr(rmatch, "match.length") - 1)
     res_align <- substring(res_align, rmatch + attr(rmatch, "match.length"))
   }
+
   styles <- c()
-  if (grepl("^[|]", lrc_data))
-    styles["border-left"] = "1px solid black"
-  if (grepl("[|]$", lrc_data))
-    styles["border-right"] = "1px solid black"
+  if (!emptyCell) {
+    if (grepl("^[|]", lrc_data))
+      styles["border-left"] = getOption("htmlTable.css.border-left",
+                                        default = getOption("htmlTable.css.border",
+                                                            default = "1px solid black"))
+    if (grepl("[|]$", lrc_data))
+      styles["border-right"] = getOption("htmlTable.css.border-right",
+                                         default = getOption("htmlTable.css.border",
+                                                             default = "1px solid black"))
+  }
 
   if (grepl("l", lrc_data))
     styles["text-align"] = "left"
