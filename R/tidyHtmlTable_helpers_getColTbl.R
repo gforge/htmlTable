7982a799eeb1c96bac6f733f8572f13616c1eb12
Max Gordon <max@gforge.se>
2020-05-24 22:25:12 +0200

    Fixed bug with arranging columns

--- R/tidyHtmlTable_helpers_getColTbl.R
+++ R/tidyHtmlTable_helpers_getColTbl.R
@@ -1,6 +1,6 @@
 getColTbl <- function(x) {
   out <- prExtractElementsAndConvertToTbl(x, elements = c("cgroup", "header")) %>%
-    dplyr::arrange() %>%
+    dplyr::arrange_all() %>%
     # This is necessary in order to not generate NA values when setting
     # hidden elements to ""
     dplyr::mutate_if(is.factor, as.character)
